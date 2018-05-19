e4ec476c3b900b8bfea327479a4db8064c9d9e6b
alinardz <alinagriselrodriguez@gmail.com>
2018-05-19 16:38:47 -0500

    done

--- starter-code/src/components/Iteration3/Iteration3.jsx
+++ starter-code/src/components/Iteration3/Iteration3.jsx
@@ -1,10 +1,28 @@
 import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
+import {Champion} from './Champion'
+import {Teams} from './teams'
+
 
 class Iteration3 extends Component{
+    state={
+        teams: Teams
+    }
+
     render(){
         return(
             <div>
-            <h1>Holi, soy Iteration3</h1>
+            <h1>NBA Champions</h1>
+                <div className="iteration1-album">
+                    {this.state.teams.map(team=>{
+                        return(
+                            <Champion key={team.id} team={team} {...team}/>
+                        );
+                    })}
+                </div>
+                <Link to='/'>
+                <h3>Back to home</h3>
+                </Link>
             </div>
         )
     }
